9b243de9d6c79c6659cd56d76c732ab0af1e6e2c
Fabio <ffirmino@live.com>
2018-05-15 16:11:00 -0300

    init

---
+++ BuscarCepMVC/Models/Cep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuscarCepMVC.Models
+{
+    public class Cep
+    {
+        [Required]
+        [MinLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        [MaxLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        public string Codigo { get; set; }
+    }
+}
